c021a7957ff0a38bee1557c90b879a922a80e455
wlin118 <linwenye02@gmail.com>
2025-06-24 20:16:03 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+Welcome to the first project in my portfolio. 
+Currently I am a recent graduate from Arizona State University, and have just started on my path to become a pentester. 
+This is a port scanner script I created that scans every port on a specified range and prints a message if the port is open. 
---
+++ port_scanner.py
@@ -0,0 +1,39 @@
+#import modules
+import sys;
+import socket;
+from datetime import datetime;
+
+#script header
+print("-"*50)
+print("Port Scanner - by Jonathan Lin")
+print("-"*50)
+
+#target
+if len(sys.argv) !=2:
+    print("input target IP")
+    sys.exit()
+target = socket.gethostbyname(sys.argv[1])
+now = datetime.now();
+
+print(f"starting scan on IP {target} at {now} for ports 1-10000")
+try:
+    #port ranges
+    for port in range(1,10000):
+        #AF_INET is address family for IPv4
+        #SOCK_STREAM is socket type for TCP
+        soc= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #timeout for when port is closed or filtered, otherwise it will hang
+        soc.settimeout(2)
+        #result returns 0 if port is open, integer if closed
+        result = soc.connect_ex((target, port))
+        if result ==0: 
+            print(f"Port {port} is open")
+        soc.close()
+except socket.gaierror:
+    #hostname invalid
+    print("Hostname could not be resolved")
+    sys.exit()
+except socket.error:
+    #server not reached
+    print("Could not connect to server")
+    sys.exit()
