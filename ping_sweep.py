23dec9cb9138fc1ce524791703ebc8258f667d91
wlin118 <linwenye02@gmail.com>
2025-07-22 21:26:24 -0700

    Update ping_sweep.py
    Finalized ping sweep for functionality

--- ping_sweep.py
+++ ping_sweep.py
@@ -9,10 +9,40 @@ print("-"*50)
 
 import subprocess
 import ipaddress
+import platform
 #get range of ip address to ping
 #start with /24 mask networks
-net_in=input("Input network/mask")
+print("Input network/mask")
+net_in=input()
 #covert string into IPv4 object
 net = ipaddress.IPv4Network(net_in)
+#check for windows or linux
+os=platform.system().lower()
+if os=="windows":
+    #windows
+    flag="-n"
+else:
+    #macOS or Linux
+    flag="-c"
+
+
 for addr in net.hosts():
-    print(addr)
+    try: 
+        str_addr=str(addr)
+        #1 ping, -W 1000 milliseconds
+        cmd= ["ping", flag, str_addr]
+        #pings each ip addr in subnet
+        #shell= true otherwise tries to find program literally named the passed string'
+        #stdout and stderrsaved into result 
+        result= subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        output= result.stdout
+        error= result.stderr
+        #result.return code seems to always return 0 with ping on my version of windows
+        if "unreachable" in output or "timed out" in output:
+            print(f"Ping failed, {str_addr} is unreachable")
+        else:
+            #stderror empty when successful
+            print(f"Ping successful, {str_addr} is reachable")
+
+    except Exception as e:
+        print(f"Error pinging {str_addr}: {e}")
